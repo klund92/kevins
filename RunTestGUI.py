8fb4364d4c68f66bc56e71e0d7a662a25fa3adef
Kevin Lund <KevinL@hoodtech.com>
2021-08-10 12:18:29 -0700

    Added Gui for Kurokesu Camera

---
+++ RunTestGUI.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  9 13:05:25 2021
+
+@author: KevinL
+"""
+
+
+import os
+import PyQt5
+import serial
+print("Current Working Directory " , os.getcwd())
+os.chdir("C:/Users/KevinL/Documents/GitHub/SCF4-SDK/src/gui_L087 (for C1_PRO_X18 camera)")
+print("Current Working Directory " , os.getcwd())
+import main.py
+# main.py
